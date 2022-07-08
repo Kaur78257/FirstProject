85db4cd640f34a33f3216fee39bd096357e73d0c
Kaur78257 <kaur78257@gmail.com>
2022-07-07 20:18:07 -0400

    new batch 44

--- src/Main2.java
+++ src/Main2.java
@@ -2,6 +2,7 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("new file Added");
         System.out.println("new file added again");
+        System.out.println("new branchgit add");
     }
 
 }
